5e8e7016cb04e2948ba8c1b8e82cfb0945b3c9c3
sahaf <sahaf@10.0.18.3>
2023-06-08 14:24:23 +0500

    2

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static String getMessage()
+    {
+        return "Hello SCD-S3 Class";
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println( Main.getMessage());
+        System.out.println( Main.getMessage());
+    }
+}
