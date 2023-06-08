a7eb49922a7ae60303377e245497a7bc845d64eb
QaisarManzoor <qaisar736@gmail.com>
2023-06-08 14:04:26 +0500

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    public static String getMessage()
+    {
+        return "Hello";
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println( Main.getMessage());
     }
 }
