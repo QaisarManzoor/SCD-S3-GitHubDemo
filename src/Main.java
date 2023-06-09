0d6331073eeb399e265f7b6ac4d5cc51c2051e6b
= <mhanzlakiani@gmail.com>
2023-06-09 15:51:01 +0500

    Commit message

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println( Main.getMessage());
+        System.out.println("Some changes");
     }
 }
